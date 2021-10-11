e1899d87c663d3031fdcafcd2be672f5d842f7db
TAN143 <nguyentantnnn82@gmail.com>
2021-10-11 15:02:57 +0700

    new project

---
+++ app/src/main/java/com/example/myproject/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myproject/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    Button btnShop;
+    TextView textView;
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.slogan);
+        logo = findViewById(R.id.logo);
+        btnShop = (Button) findViewById(R.id.btnShop);
+
+        Animation animation = AnimationUtils.loadAnimation(WelcomeActivity.this, R.anim.fadein);
+
+        logo.startAnimation(animation);
+        btnShop.startAnimation(animation);
+        textView.startAnimation(animation);
+
+        btnShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
