4f6c2a34a595067fed8a06d0be1e09f8de0ff6be
TAN143 <nguyentantnnn82@gmail.com>
2021-10-15 14:45:10 +0700

    sthing in bottom navigation

--- app/src/main/java/com/example/myproject/HomeActivity.java
+++ app/src/main/java/com/example/myproject/HomeActivity.java
@@ -1,15 +1,24 @@
 package com.example.myproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.myproject.fragment.TrendFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    BottomNavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +26,61 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         toolbar = findViewById(R.id.main_toolbar);
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("RADAR");
 
+        navigation = findViewById(R.id.bottom_navigation);
 
+        loadFragment(new TrendFragment());
+
+        navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment;
+
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportActionBar().setTitle("RADAR");
+                        fragment = new TrendFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.search:
+                        getSupportActionBar().setTitle("SEARCH");
+                        fragment = new TrendFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.favourite:
+                        getSupportActionBar().setTitle("FAVOURITE");
+                        fragment = new TrendFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.shoppingbag:
+                        getSupportActionBar().setTitle("SHOPPING BAG");
+                        fragment = new TrendFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.account:
+                        getSupportActionBar().setTitle("ACCOUNT");
+                        fragment = new TrendFragment();
+                        loadFragment(fragment);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction =
+                getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     @Override
