5a9ba5ba311000f8e146acb2ee15142e3a2e084b
TAN143 <nguyentantnnn82@gmail.com>
2021-10-17 14:24:23 +0700

    completed UI intro

--- app/src/main/java/com/example/myproject/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/myproject/activity/WelcomeActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,6 +19,9 @@ public class WelcomeActivity extends AppCompatActivity {
     Button btnShop;
     TextView textView;
     ImageView logo;
+    TextView slogan1;
+    TextView slogan2;
+    TextView slogan3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +30,35 @@ public class WelcomeActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.slogan);
         logo = findViewById(R.id.logo);
-        btnShop = (Button) findViewById(R.id.btnShop);
+        btnShop = findViewById(R.id.btnShop);
+        slogan1 = findViewById(R.id.slogan1);
+        slogan2 = findViewById(R.id.slogan2);
+        slogan3 = findViewById(R.id.slogan3);
 
-        Animation animation = AnimationUtils.loadAnimation(WelcomeActivity.this, R.anim.fadein);
+        Animation aSlogan1 = AnimationUtils.loadAnimation(WelcomeActivity.this, R.anim.fadein);
+        Animation aSlogan2 = AnimationUtils.loadAnimation(WelcomeActivity.this, R.anim.fadein);
+        Animation aSlogan3 = AnimationUtils.loadAnimation(WelcomeActivity.this, R.anim.fadein);
+
+        Animation animation = AnimationUtils.loadAnimation(WelcomeActivity.this, R.anim.fadeinlogo);
+        Animation animation1 = AnimationUtils.loadAnimation(WelcomeActivity.this, R.anim.fadeinlogo);
+        Animation animation2= AnimationUtils.loadAnimation(WelcomeActivity.this, R.anim.fadeinlogo);
+
+        aSlogan1.setDuration(4000);
+        aSlogan2.setStartOffset(4000);
+        aSlogan3.setStartOffset(8000);
+
+
+        animation.setStartOffset(13000);
+        animation1.setStartOffset(13000);
+        animation2.setStartOffset(13000);
+
+        slogan1.startAnimation(aSlogan1);
+        slogan2.startAnimation(aSlogan2);
+        slogan3.startAnimation(aSlogan3);
 
         logo.startAnimation(animation);
-        btnShop.startAnimation(animation);
-        textView.startAnimation(animation);
+        btnShop.startAnimation(animation1);
+        textView.startAnimation(animation2);
 
         btnShop.setOnClickListener(new View.OnClickListener() {
             @Override
